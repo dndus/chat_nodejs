be8b174cfd7b1372622842c09be24b400da9199a
dndus <onenavero@gmail.com>
2022-03-14 16:35:28 +0500

    update

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const formatMessage = require('./utils/messages')
+const formatMessage = require('./utils/messages');
+const {userJoin, GetCurrentUser} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,17 +15,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Run when client connects
 io.on("connection", socket => {
     socket.on('joinroom', ({username, room}) => {
+        const user = userJoin(socket.id, username, room)
+
+        socket.join(user.room)
+
     // Welcome current user
     socket.emit('message', formatMessage('Chat bot', 'Welcome to Chat'));
 
     //Broadcast when a user connects
-    socket.broadcast.emit('message', formatMessage('Chat bot', 'A user has joined the chat'));
+    socket.broadcast.to(user.room).emit('message', formatMessage('Chat bot', `${user.username} has joined the chat`));
 
     })
 
     //Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', formatMessage('USER', msg))
+        const user = GetCurrentUser(socket.id)
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
     })
 
     // Runs when client left
